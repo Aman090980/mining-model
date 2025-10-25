f2843d07050887d92047dba5331e5d822e3db78c
Aman090980 <amandeepmohanty1@gmail.com>
2025-10-26 01:33:07 +0530

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const btnSwitch = document.querySelector('#theme');
-btnSwitch.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    btnSwitch.classList.toggle('active');
-})
